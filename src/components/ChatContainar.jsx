1bd6ccf3ce4edbc6934ee3acc8cb2e581a728581
rakeshkhaarol <kharol64666@gmail.com>
2025-08-23 15:45:13 +0530

    chatcontaint some work

--- src/components/ChatContainar.jsx
+++ src/components/ChatContainar.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import assets, { messagesDummyData } from '../assets/assets'
+import { FormatMessageTime } from '../lib/Utils'
 
-function ChatContainar() {
-  return (
-    <div>ChatContainar</div>
+function ChatContainar({ selectedUser, setSelectedUser }) {
+  const scrollEnd = useRef()
+
+  useEffect(() => {
+    if (scrollEnd.current) {
+      scrollEnd.current.scrollIntoView({ behavior: "smooth" })
+    }
+  }, [])
+  return selectedUser ? (
+    <div className='h-full overflow-scroll relative backdrop-blue-lg'>
+      {/* ---------header area ------------ */}
+      <div className='flex items-center text-white gap-3 py-3 mx-4 border-b border-stone-500'>
+        <img className='w-8 rounded-full' src={assets.profile_martin} alt='' />
+        <p className='flex-1 text-lg text-white flex items-center gap-2'>
+          martin johan
+          <span className='w-2 h-2 bg-green-500 rounded-full'></span>
+        </p>
+        <img onClick={() => { setSelectedUser(null) }} src={assets.arrow_icon} alt='' className='md:hidden max-w-7' />
+        <img src={assets.help_icon} className='max-md:hidden max-w-5' />
+      </div>
+      {/* -----chat area ------ */}
+      <div className='flex flex-col items-center h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6'>
+        {
+          messagesDummyData.map((msg, idx) => (
+            <div key={idx} className={`flex items-end gap-2 justify-end ${msg.senderId !== '680f50aaf10f3cd28382ecf2' && 'flex-row-reverse'}`}>
+              {
+                msg.image ? (
+                  <img src={msg.image} alt='' className='max-w-[230px] border border-gray-700 overflow-hidden mb-8' />
+                ) : (
+                  <p className={`p-2 max-w-[200px] md:text-sm font-light
+                     rounded-lg mb-8 break-all bg-violet-500/30 
+                     text-white ${msg.senderId === '680f50aaf10f3cd28382ecf2'
+                      ? 'rounded-br-none' : 'rounded-bl-none'}`}>{msg.text}</p>
+                )
+              }
+              <div className='text-center text-xs'>
+                <img src={msg.senderId === '680f50aaf10f3cd28382ecf2' ? assets.avatar_icon : assets.profile_martin} alt='' className='w-5 rounded-full' />
+                <p className='text-gray-500'>{FormatMessageTime(msg.createdAt)}</p>
+              </div>
+
+            </div>
+          ))
+        }
+        <div ref={scrollEnd}></div>
+
+      </div>
+    </div>
+  ) : (
+    <div className='flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden'>
+      <img src={assets.logo_icon} alt='' className="max-w-16" />
+      <p className='text-white font-medium text-lg'>chat anytime anywhere</p>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,27 +4,25 @@ import Sidebar from '../components/Sidebar'
 import ChatContainar from '../components/ChatContainar'
 import RightSidebar from '../components/RightSidebar'
 
-//2. definetion area
+//2. definition area
 function Home() {
     //2.1 hooks area
     const [selectedUser, setSelectedUser] = useState(false)
 
-
-    //2.2 function definetion area
-
-
-
-    //2.3 return statment
+    //2.3 return statement
     return (
-        <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
-            <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-md 
-                overflow-hidden h-[100%] grid grid-cols-2 relative
-                 ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]'
-                    : 'md-grid-cols-2'}`}>
-
-                <Sidebar />
-                <ChatContainar />
-                <RightSidebar />
+        <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+            <div
+                className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl
+                overflow-hidden h-full grid grid-cols-3 relative
+                ${selectedUser
+                    ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]'
+                    : 'md:grid-cols-2'
+                }`}
+            >
+                <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+                <ChatContainar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+                <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
             </div>
         </div>
     )

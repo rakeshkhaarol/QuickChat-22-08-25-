817dd61dfb37186ce882ba349274f6d62d497ad3
rakeshkhaarol <kharol64666@gmail.com>
2025-08-25 13:01:15 +0530

    c3

--- src/components/ChatContainar.jsx
+++ src/components/ChatContainar.jsx
@@ -23,22 +23,24 @@ function ChatContainar({ selectedUser, setSelectedUser }) {
         <img src={assets.help_icon} className='max-md:hidden max-w-5' />
       </div>
       {/* -----chat area ------ */}
-      <div className='flex flex-col items-center h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6'>
+      <div className='flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6'>
         {
           messagesDummyData.map((msg, idx) => (
-            <div key={idx} className={`flex items-end gap-2 justify-end ${msg.senderId !== '680f50aaf10f3cd28382ecf2' && 'flex-row-reverse'}`}>
+            <div key={idx} className={`flex items-end gap-2 justify-end ${msg.senderId !== '680f50e4f10f3cd28382ecf9' && 'flex-row-reverse'}`}>
               {
                 msg.image ? (
-                  <img src={msg.image} alt='' className='max-w-[230px] border border-gray-700 overflow-hidden mb-8' />
+                  <img src={msg.image} alt='' className='max-w-[230px] border border-gray-700 overflow-hidden rounded-lg mb-8' />
                 ) : (
                   <p className={`p-2 max-w-[200px] md:text-sm font-light
                      rounded-lg mb-8 break-all bg-violet-500/30 
-                     text-white ${msg.senderId === '680f50aaf10f3cd28382ecf2'
+                     text-white ${msg.senderId === '680f50e4f10f3cd28382ecf9'
                       ? 'rounded-br-none' : 'rounded-bl-none'}`}>{msg.text}</p>
                 )
               }
               <div className='text-center text-xs'>
-                <img src={msg.senderId === '680f50aaf10f3cd28382ecf2' ? assets.avatar_icon : assets.profile_martin} alt='' className='w-5 rounded-full' />
+                <img src={msg.senderId === '680f50e4f10f3cd28382ecf9' ?
+                   assets.avatar_icon : assets.profile_martin} alt='' 
+                   className='w-5 rounded-full' />
                 <p className='text-gray-500'>{FormatMessageTime(msg.createdAt)}</p>
               </div>
 
@@ -46,7 +48,19 @@ function ChatContainar({ selectedUser, setSelectedUser }) {
           ))
         }
         <div ref={scrollEnd}></div>
+      </div>
+      {/* ---------buttom area ----------- */}
 
+      <div className='absolute bottom-0 right-0 left-0 flex items-center gap-3 p-3'>
+        <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
+          <input type='text' placeholder='send a message...'
+          className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400'/>
+          <input type="file" id='image' accept='image/png, image/jpeg' hidden />
+          <label htmlFor="image">
+            <img src={assets.gallery_icon} alt="" className='w-5 mr-2 cursor-pointer' />
+          </label>
+        </div>
+        <img src={assets.send_button} className='w-7 cursor-pointer' />
       </div>
     </div>
   ) : (
